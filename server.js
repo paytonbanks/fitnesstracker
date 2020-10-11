2d48e479eb9e508b025f176111a495c8fa125db9
Payton Banks <paytonbanks@Mac-Payton.local>
2020-10-11 16:56:41 -0400

    stablilzed server, replaced singles quotes with 'backtics', imported linked pages, ran Lighthouse

--- server.js
+++ server.js
@@ -7,20 +7,18 @@ const app = express();
 app.use(logger("dev")); 
 app.use(express.json());
 app.use(express.static("public"));
-app.use(express.urlencoded(
-    { extedned: true }
-));
+app.use(express.urlencoded({ extedned: true }));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false
 });
 
-app.use(require("./routes/api-routes.js")); 
-app.use(require("./routes/html.js"));
+// app.use(require("./routes/api-routes")); 
+app.use(require("./routes/html"));
 
 app.listen(PORT, () => {
-    console.log('App running on ${PORT}');
+    console.log(`App running on  ${PORT}!`);
 });

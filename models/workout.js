534f49bb8012271ad016d773d8f242b7bf4ab0b0
Payton Banks <paytonbanks@Mac-Payton.local>
2020-10-13 00:09:16 -0400

    routes updated

--- models/workout.js
+++ models/workout.js
@@ -1,22 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
-
-// As a user, I want to be able to view create and track daily workouts. 
-// I want to be able to log multiple exercises in a workout on a given day. 
-// I should also be able to track the 
-// name, type, weight, sets, reps, and duration of exercise. 
-// If the exercise is a cardio exercise, 
-// I should be able to track my distance traveled.
-
-
 const WorkoutSchema = new Schema({
     day: {
         type: Date, 
         default: Date.now,
     },
-    exercises: [
+    exercise: [
         {
             type: {
                 type: String,
@@ -48,8 +38,6 @@ const WorkoutSchema = new Schema({
 
     ], 
 
-    // date
-    // exercises:[name, type, weight, sets, reps, and duration of exercise. ]
 
 });
 
--- seeders/seed.js
+++ seeders/seed.js
@@ -3,7 +3,7 @@ let db = require("../models");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  
 });
 
 let workoutSeed = [

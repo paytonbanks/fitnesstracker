e2b7bd3de1ef996ed28d48203814276793324e01
Payton Banks <paytonbanks@Mac-Payton.local>
2020-10-12 23:36:58 -0400

    updated seeds file and heroku auto update

--- server.js
+++ server.js
@@ -4,21 +4,16 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(logger("dev")); 
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
-    useNewUrlParser: true,
-    // useUnifiedTopology: true,
-    useCreateIndex: true,
-    // useFindAndModify: false
-});
 
-// app.use(require("./routes/api-routes")); 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
 app.use(require("./routes/api-routes.js"));
-app.use(require("./routes/html.js"));
+app.use(require("./routes/html-routes.js"));
 
 
 app.listen(PORT, () => {
